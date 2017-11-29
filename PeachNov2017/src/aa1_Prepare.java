79c8ce40a343b434126853755b257133d226a7c0
Kosireddi <Kosireddi@DESKTOP-NQIIQ77>
2017-11-29 09:22:18 -0800

    New project for Dev team to work

---
+++ PeachNov2017/src/aa1_Prepare.java
@@ -0,0 +1,28 @@
+public class aa1_Prepare {
+
+	public static void main(String[] args) {
+	//		Read and Understand the o/p from AA Test Management (xl2Selected.xls)
+		
+		//		TRB - TestRun Block
+		//		Breaking the TestRun into a block of executable run. 
+		// 		1 independent piece where all the information is there to execute an "end to end test run". Smallest executable block or test case or user story. testRunBlock
+		//		Multiple testRunBlocks make 1 testRun.
+		
+		//		trb - Object Array.
+		
+
+		
+		//		Logic :
+		//		Read the excel sheets.
+		//		Go through each row of Mod, TC and list down the TC+TD that needs to get executed.
+		//		Each combination becomes 1 testRunBlock.
+		//		Get all the required information and put it into each tRB. 
+		//		Then the execution can get started with 1 tRB at a time for single execution or multiple for parallel. 
+		//		After the end of this program, we start the next one (selExecuteRun.java
+
+		
+		
+
+	}
+
+}
---
+++ PeachNov2017/src/aa2_Execute.java
@@ -0,0 +1,24 @@
+
+public class aa2_Execute {
+
+	public static void main(String[] args) {
+
+//		“xl3Expanded.xls”
+//		Logic:
+//		1. Read all the Run parameters
+		// Q. What and where are the Run Parameters
+		// ReqID? Trello link? 
+		
+		
+//		2. Start with the 1st tRB
+		
+		
+//		Call the corresponding SWD KW and start execution.
+//		Report details of execution.
+//		Repeat till end of all tRB.
+		
+		
+
+	}
+
+}
